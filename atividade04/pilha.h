2d885cf3c90d172c759ad0200fd624c247a396d1
Heber Leandro <heberleandroluz@gmail.com>
2019-09-02 14:43:30 -0300

    atividade04

---
+++ atividade04/pilha.h
@@ -0,0 +1,20 @@
+#ifndef _pilha_H_
+#define _pilha_H_
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Pilha {
+	
+	private:
+		int capacidade, topo;	
+		
+	public:
+		Pilha(int capacidade){
+					
+		}
+};
+
+
+#endif
