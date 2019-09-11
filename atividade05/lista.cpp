1fd78ef24d4d6630d94a6cb6d7a0b7ed77f471ff
unknown <a>
2019-09-10 21:40:00 -0300

    atividade05

---
+++ atividade05/lista.cpp
@@ -0,0 +1,36 @@
+/*
+ * lista.cpp
+ *
+ *  Created on: 28 de set de 2017
+ *      Author: ramide
+ */
+#include <iostream>
+#include "lista2.h"
+using namespace std;
+
+int main3() {
+	Lista<char> lista(10);
+
+	lista.adiciona('a');	// a
+	lista.adiciona('b');	// a b
+	lista.adiciona('c');	// a b c
+	lista.adiciona('d');	// a b c d
+	lista.adiciona('e');	// a b c d e
+	lista.exibe(); 			// a b c d e (saída)
+
+	lista.remove(3); 		// a b d e
+	lista.exibe(); 			// a b d e (saída)
+
+	cout << lista.pega(4) << endl; 			// e
+	lista.insere(2, 'f'); 	// a f b d e
+	lista.exibe(); 			// a f b d e (saída)
+
+	lista.adiciona('g'); 	// a f b d e g
+	lista.insere(4, 'h'); 	// a f b h d e g
+	cout << lista.pega(2) << endl; 			// f
+	lista.exibe(); 			// a f b h d e g (saída)
+	
+	return 0;
+}
+
+
---
+++ atividade05/pilha2.h
@@ -0,0 +1,63 @@
+#ifndef _pilha2_H_
+#define _pilha2_H_
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+struct Node {
+	T dados;
+	struct Node * prox;
+};
+
+template <class T>
+class Pilha {
+	
+	private:
+		int capacidade;
+		int Nitems; // Quantidade de Nós na pilha.
+		Node<T> *topo;
+		
+		
+	public:
+		Pilha(int capacidade){
+			this->topo = NULL;
+			this->capacidade = capacidade;
+			this->Nitems = 0;		
+		}
+		
+		~Pilha(){
+			while(this->topo != NULL){
+				Node<T> *aux = this->topo;
+				this->topo = this->topo->prox;
+				delete aux;
+			}
+			delete topo;
+		}
+		
+		void empilha(T item) {
+			if(this->Nitems < this->capacidade){
+				Node<T> *novoNode = new Node<T>;
+				novoNode->dados = item;
+				novoNode->prox = this->topo;
+				this->topo = novoNode;	
+				this->Nitems++;
+			}else{
+				cout << "Overflow!" << endl;
+			}
+		}
+		
+		T desempilha(){
+			if(this->Nitems > 0){
+				Node<T> *aux = this->topo;
+				this->topo = this->topo->prox;
+				this->Nitems--;			
+				return aux->dados;
+			}else {
+				cout << "Underflow!" << endl;
+			}
+		}
+};
+
+
+#endif
